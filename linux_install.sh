db3c9c1216b2fcf5d8dbc8fa2faf6bad8733cfb3
wongxingjun <wongxingjun@126.com>
2016-03-24 13:26:39 +0800

    update

--- linux_install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo apt-get install git vim gcc g++ cmake
-sudo add-apt-repository ppa:numix/ppa
-sudo apt-get update
-sudo apt-get install numix-gtk-theme numix-icon-theme-circle
-sudo apt-get install numix-wallpaper-*
-sudo apt-get install gnome-tweak-tool
